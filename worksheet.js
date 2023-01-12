a073ea25ccb53e2c77c68100cff4600da2aa2aee
rudranilK <rudranilkundu@gmail.com>
2023-01-12 14:40:55 +0530

    solution for filter method

--- worksheet.js
+++ worksheet.js
@@ -1,33 +1,7 @@
-const characters = [
-    {
-        name: 'Luke Skywalker',
-        height: 172,
-        mass: 77,
-        eye_color: 'blue',
-        gender: 'male',
-    },
-    {
-        name: 'Darth Vader',
-        height: 202,
-        mass: 136,
-        eye_color: 'yellow',
-        gender: 'male',
-    },
-    {
-        name: 'Leia Organa',
-        height: 150,
-        mass: 49,
-        eye_color: 'brown',
-        gender: 'female',
-    },
-    {
-        name: 'Anakin Skywalker',
-        height: 188,
-        mass: 84,
-        eye_color: 'blue',
-        gender: 'male',
-    },
-];
+const fs = require('fs');
+const path = require('path');
+const characters = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'data.json'), 'utf-8'));
+
 
 //***MAP***
 //1. Get array of all names
@@ -43,9 +17,16 @@ const characters = [
 
 //***FILTER***
 //1. Get characters with mass greater than 100
+// console.log(characters.filter(e => e['mass'] > 100));
+
 //2. Get characters with height less than 200
+// console.log(characters.filter(e => e['height'] < 200));
+
 //3. Get all male characters
+// console.log(characters.filter(e => e['gender'].toLowerCase() === 'male'));
+
 //4. Get all female characters
+// console.log(characters.filter(e => e['gender'].toLowerCase() === 'female'));s
 
 //***SORT***
 //1. Sort by mass
